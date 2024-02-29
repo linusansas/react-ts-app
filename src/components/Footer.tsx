047e8f9307692cacbd6ad1bc2007b2366fdafd91
linusansas <linusansas@gmail.com>
2024-02-29 22:36:27 +0100

    Update footer text in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
    return (
       <footer className="bg-gray-100 text-gray-400 fixed bottom-0 w-full flex justify-center">
-         <p>Footer</p>
+         <p className="font-thin mt-2">Created by Linus Ansas using Giphy-Api</p>
       </footer>
    );
 }
